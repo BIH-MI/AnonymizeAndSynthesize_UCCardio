132f8ac02cf61b4d5843afe36cca4043957fa62b
karen-otte <karen.otte@charite.de>
2024-08-14 15:39:07 +0200

    Added BSL where appropriate

--- anonymization/java_code/src/main/java/org/bihmi/usecase_cardiology/IO.java
+++ anonymization/java_code/src/main/java/org/bihmi/usecase_cardiology/IO.java
@@ -2,12 +2,11 @@
  * Use Case Cardiology HiGHmed Data Anonymisation
  * Copyright (C) 2023 - Berlin Institute of Health
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * Use of this software is govered by the Business Source License included in the LICENSE.md file.
+ * Change Date: 14.08.2028, Change License: Academic Free License v3.0
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the
+ * open source license AFL v3.0 or as specified in the LICENSE.md file.
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- evaluation/evaluation_script.py
+++ evaluation/evaluation_script.py
@@ -1,3 +1,19 @@
+# /**
+#  * Use Case Cardiology HiGHmed Data Anonymisation
+#  * Copyright (C) 2023 - Berlin Institute of Health
+#  * <p>
+#  * Use of this software is govered by the Business Source License included in the LICENSE.md file.
+#  * Change Date: 14.08.2028, Change License: Academic Free License v3.0
+#  * On the date above, in accordance with the Business Source License, use of this software will be governed by the
+#  * open source license AFL v3.0 or as specified in the LICENSE.md file.
+#  *
+#  * Unless required by applicable law or agreed to in writing, software
+#  * distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+#  */
+
 from argparse import ArgumentParser
 from datetime import datetime
 
